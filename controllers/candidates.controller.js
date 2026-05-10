3b545e714c5aa54126ec025acf730ef936761b93
elmasha <elmashavitran@gmail.com>
2026-05-10 22:12:02 +0300

    SD

--- controllers/candidates.controller.js
+++ controllers/candidates.controller.js
@@ -1,5 +1,6 @@
 const db = require("../config/db");
 const redis = require("../config/redis");
+const query = utils.promisify(db.query).bind(db);
 const {
   candidateKey,
   candidatesAvailableKey,
@@ -254,7 +255,7 @@ exports.updateCandidate = async (req, res) => {
         bureau_no,
         working_status || "available",
         status || "Available",
-        id,
+        id, 
       ]
     );
 
