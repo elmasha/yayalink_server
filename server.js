5e3fa8ed6841d74b64cdd1ae81c49eab89afbef3
elmasha <elmashavitran@gmail.com>
2026-05-16 19:32:24 +0300

    SD

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use((req, res, next) => {
 });
 
 app.use("/api/auth", require("./routes/auth.routes"));
+app.use("/api/admin", require("./routes/admin.routes"));
 app.use("/api/notifications", require("./routes/notification.routes"));
 app.use("/api/users", require("./routes/users.routes"));
  app.use("/api/bureaus", require("./routes/bureau.routes"));
